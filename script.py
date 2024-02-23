c83f16534a01d35354329b790b1de8daa9d31960
Chloe2330 <lin1577@purdue.edu>
2024-02-23 11:01:06 -0500

    Option to clear output files

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 import subprocess
 import sys
 import os
+import shutil
 
 if (__name__ != "__main__"):
     exit(1)
@@ -10,10 +11,37 @@ if (len(sys.argv) < 2):
     print("Please input an argument")
     exit(1)
 
+# Usage of script 
 if (sys.argv[1] == "--help"):
     print("This script will run commands on every repository in its directory.")
     print("Usage: script.py \"command 1\" \"command 2\" \"command 3\" ...")
     print("Example: ./script.py \"actionlint\" \"ls -a\"")
+    exit(0)
+
+# Clear output files 
+if (sys.argv[1] == "--clear"):
+    print("This script will clear out output text files in the results directory.")
+    user_input = input("Do you want to execute this command? (y/n): ").lower()
+
+    # User confirms deletion 
+    if user_input == 'y':
+        current_folder = os.getcwd()
+        results_path = os.path.join(current_folder, "results")
+
+        # Check if results folder exists 
+        if os.path.exists(results_path):
+            for item in os.listdir(results_path):
+                item_path = os.path.join(results_path, item)
+                if os.path.isfile(item_path):
+                    os.remove(item_path)
+                    print(f"Deleted file: {item_path}")
+                elif os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
+                    print(f"Deleted directory: {item_path}")
+        else:
+            print("No results to delete.")
+    else:
+        print("Exiting...")
     exit(0)
 
 # Access the script's directory and get all of its repositories
@@ -25,6 +53,7 @@ for item in os.listdir('.'):
 
 def run(command, repo):
     temp_file_path = os.path.join(os.getcwd(), "temp.txt")
+
     with open(temp_file_path, 'w') as file:
         output = subprocess.run(command, shell=True, stdout=file)
         move_files(temp_file_path, repo, command)
@@ -39,9 +68,14 @@ def run(command, repo):
 def move_files(temp, repo, command):
     os.chdir("..")
     folder_name = os.path.join('results', repo)
+
+    # Create results folder if it does not exist
     os.makedirs(folder_name, exist_ok=True)
+
     file_name = f"{command.replace(' ', '_')}.txt"
     file_path = os.path.join(folder_name, file_name)
+
+    # Move output from temp path to results path 
     os.replace(temp, file_path)
 
 # Execute the command on every repository in the folder
